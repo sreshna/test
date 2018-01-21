4b30a9ec0c28429676b9a5e33d9b86058b725162
Pallav Parikh <Pallav@melvault.com>
2018-01-21 14:03:49 +0530

    added a name field in user model

--- api/models.py
+++ api/models.py
@@ -8,3 +8,7 @@ from django.db import models
 class User(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+	return self.name
